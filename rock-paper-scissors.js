f17601826f174a3ff4e91049f06715c27ff871d1
unknown <dmphuc21@clc.fitus.edu.vn>
2023-10-21 20:26:48 +0700

    playround function - compare the choice of player and computer and return the result

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -4,3 +4,24 @@ function getComputerChoice(){
     let randomIndex = Math.floor(Math.random() * choices.length);
     return choices[randomIndex];
 }
+function playRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase();
+    computerSelection = computerSelection.toLowerCase();
+    if (playerSelection == computerSelection)
+        return "It's a tie! Both chose " + playerSelection;
+    else if ((playerSelection == "rock" && computerSelection == "scissors") ||
+            (playerSelection == "paper" && computerSelection == "rock") ||
+            (playerSelection == "scissors" && computerSelection == "paper")) {
+                return "You Win! " +  playerSelection + " beats " +  computerSelection;
+           }
+    else
+        return "You Lose! " +  comptuterSelection + " beats " +  playerelection;
+       
+}
+function game(){
+    for (let i =0;i<5;i++){
+        comptuterSelection = getComputerChoice();
+        playerSelection = prompt();
+        console.log(playRound(playerSelection,comptuterSelection));
+    }
+}
